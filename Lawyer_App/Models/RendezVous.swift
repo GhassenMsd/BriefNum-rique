abca237de14c13f41746a639e8ad90e98632ef1c
Ghassen Msaad <ghassenmsaad3@gmail.com>
2019-12-16 21:57:50 +0100

    fix warning

---
+++ Lawyer_App/Models/RendezVous.swift
@@ -0,0 +1,26 @@
+//
+//  RendezVous.swift
+//  Lawyer_App
+//
+//  Created by hamadi aziz on 12/12/2019.
+//  Copyright © 2019 hamadi aziz. All rights reserved.
+//
+import UIKit
+
+class RendezVous: NSObject {
+    var id = 0
+    var date = ""
+    var adresse = ""
+    var sujet = ""
+    var notes = ""
+    var id_Av = 0
+    
+    init(id: Int, date: String, adresse: String, sujet: String, notes: String, id_Av: Int){
+        self.id = id
+        self.date = date
+        self.adresse = adresse
+        self.sujet = sujet
+        self.notes = notes
+        self.id_Av = id_Av
+    }
+}
